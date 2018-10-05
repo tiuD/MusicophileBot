4c297f97aec4b1832bbf588f11f9072b979fec8e
Amir A. Shabani <amirshabani011@gmail.com>
2018-10-05 11:55:39 +0330

    Number of genres and users in /stats command

--- main.py
+++ main.py
@@ -249,6 +249,8 @@ def top(bot, update, args):
 def stats(bot, update):
     try:
         hashtags_count = 0
+        distinct_genres = set()
+        distinct_users = set()
         stats_statement = ''
         client = MongoClient('localhost', 27017)
         db = client[config.DB_NAME]
@@ -257,10 +259,16 @@ def stats(bot, update):
         
         for song in songs: 
             hashtags_count += len(song['genres'])
+            distinct_genres.update(song['genres'])
+        
+        for v in votes:
+            distinct_users.update([v['user_id']])
         
         stats_statement += 'Number of songs: {}\n'.format(songs.count())
         stats_statement += 'Number of hashtags: {}\n'.format(hashtags_count)
+        stats_statement += 'Number of genres: {}\n'.format(len(distinct_genres))
         stats_statement += 'Number of votes: {}\n'.format(votes.count())
+        stats_statement += 'Number of users: {}\n'.format(len(distinct_users)) 
         update.message.reply_text(stats_statement)
     except Exception as e:
         print(e)
