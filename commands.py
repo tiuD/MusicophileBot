98b7b5dd1b10255cbc5e65ea965c413d8559752e
Amir A. Shabani <amirshabani011@gmail.com>
2019-02-26 10:13:30 +0330

    moved /top to commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
 from telegram import ParseMode
 from pymongo import MongoClient
 from config import config
+from collections import Counter
+from settings import VOTE_EMOJIS
 
 def start(bot, update):
     statement = 'Hey! Welcome to *MusicophileBot*!\n'
@@ -42,3 +44,69 @@ def stats(bot, update):
         print(e)
     finally:
         client.close()
+
+
+def top(bot, update, args):
+    text = ''
+    client = MongoClient('localhost', 27017)
+    db = client[config('database.ini', 'mongodb')['db_name']]
+    if (args[0] == 'genres'):
+        hashtags = []
+
+        count = int(args[1]) if(len(args) > 1) else 10
+
+        songs = db['Songs'].find({})
+
+        for song in songs:
+            hashtags.extend(song['genres'])
+
+        freq = Counter(hashtags)
+
+        i = 1
+        for item in freq.most_common(count):
+            text += '{}. {}: {} time{}\n'.format(i, item[0], item[1], ('s' if (item[1] > 1) else ''))
+            i += 1
+
+        update.message.reply_text(text)
+    elif (args[0] == 'songs'):
+        scores = {}
+
+        songs = db['Songs'].find({})
+
+        for song in songs:
+            score = (
+                     (song['votes']['heart'] * 2) +
+                     (song['votes']['like']) +
+                     (song['votes']['dislike'] * (-1)) +
+                     (song['votes']['poop'] * (-2))
+                    )
+            scores[song['name']] = (song['song_id'], score, song['votes'])
+
+        top_songs = sorted(scores.items(), key=lambda x:x[1][1], reverse=True)
+
+        try:
+            count = (int(args[1]) if(int(args[1]) < len(top_songs)) else len(top_songs)) if(len(args) > 1) else 10
+        except Exception as e:
+            print(e)
+
+        i = 0
+        while i < count:
+            song = top_songs[i]
+            heart = song[1][2]['heart']
+            like = song[1][2]['like']
+            dislike = song[1][2]['dislike']
+            poop = song[1][2]['poop']
+
+            text += '{}. [{}](https://t.me/musicophileowl/{}): {}{}{}{}{}{}{}{}{}\n'.format(
+                    i+1, song[0], song[1][0],
+                    VOTE_EMOJIS['heart'] if (heart > 0) else '', '{} '.format(heart) if(heart > 0) else '',
+                    VOTE_EMOJIS['like'] if (like > 0) else '', '{} '.format(like) if(like > 0) else '',
+                    VOTE_EMOJIS['dislike'] if (dislike > 0) else '', '{} '.format(dislike) if(dislike > 0) else '',
+                    VOTE_EMOJIS['poop'] if (poop > 0) else '', '{} '.format(poop) if(poop > 0) else '',
+                    'no votes yet' if((heart + like + dislike + poop) == 0) else ''
+                )
+            i += 1
+
+        update.message.reply_text(text,
+                                  parse_mode=ParseMode.MARKDOWN,
+                                  disable_web_page_preview=True)
